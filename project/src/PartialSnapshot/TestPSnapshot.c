d722911b24057d3fdaa8d5906045e1cd9c2aa511
serb1231 <serban.ionescu15@gmail.com>
2023-06-17 13:57:16 +0200

    Tests with assertions ready for 64 threads

--- project/src/PartialSnapshot/TestPSnapshot.c
+++ project/src/PartialSnapshot/TestPSnapshot.c
@@ -4,6 +4,8 @@
 #include "PSnapshot.h"
 #include <time.h>
 
+#define MAXTHRD 64
+
 int main(){
 
     // Test 1
@@ -43,11 +45,11 @@ int main(){
 
 
     // Test 2:
-    printf("Test 2: Snapshot made for 2,4,8 threads, initialized with value 1 in the registers.\n \
+    printf("Test 2: Snapshot made for 2,4,8,16,32,64 threads, initialized with value 1 in the registers.\n \
      * Assertion 1: a scan performed after the initialization returns an array of 1s\n \
      * Assertion 2: Every thread writes its id in its register. A susequent scan returns an arrays with the threadIDs\n");
 
-    for (int i = 2; i <= 8; i = i * 2) {
+    for (int i = 2; i <= MAXTHRD; i = i * 2) {
         printf("\nTest 2 with %d threads.\n",i);
         PSnapshot test2Snapshot;
         if(createPSnapshot(&test2Snapshot, i, i, 1) == EXIT_FAILURE){
@@ -91,11 +93,11 @@ int main(){
 
 
     // Test 3:
-    printf("Test 3: Snapshot made for 2,4,8 threads. Thread 0 scans, sleeps for 10 milliseconds, scans again;\n \
+    printf("Test 3: Snapshot made for 2,4,8,16,32,64 threads. Thread 0 scans, sleeps for 10 milliseconds, scans again;\n \
      the other threads updates their register with values from 0 to 1000.\n \
      * Assertion: the second scan must have greater values than the first scan, element-wise.\n");
 
-    for (int i = 2; i <= 8; i = i * 2) {
+    for (int i = 2; i <= MAXTHRD; i = i * 2) {
         printf("\nTest 3 with %d threads.\n",i);
         PSnapshot test3Snapshot;
         if(createPSnapshot(&test3Snapshot, 100, 100,0) == EXIT_FAILURE){
@@ -134,10 +136,10 @@ int main(){
         for (int j = 0; j < i; ++j) {
             printf("[%d] ", snap2[j]);
         }
-        // for (int j = 0; j < i; ++j) {
-        //     if(snap1[j] > snap2[j])
-        //         assert(false);
-        // }
+        for (int j = 0; j < i; ++j) {
+            if(snap1[j] > snap2[j])
+                assert(false);
+        }
         printf("\nTest 3 with %d threads has passed.\n",i);
         freePSnapshot(&test3Snapshot);
 
@@ -145,12 +147,12 @@ int main(){
     printf("\nTest 3 passed.\n\n");
 
     // Test 4:
-    printf("Test 4: Snapshot made for 4,8,12 threads. \n \
+    printf("Test 4: Snapshot made for 4,8,12,..,64 threads. \n \
     Thread 0 scans, thread 1 scans, sleeps for 10 milliseconds, scans again, finally thread 0 scans again;\n \
     The other threads updates their register with values from 0 to 1000.\n \
     * Assertion: the second scan must have greater values than the first scan, element-wise.\n");
 
-    for (int i = 4; i <= 12; i += 4) {
+    for (int i = 4; i <= MAXTHRD; i += 4) {
         printf("\nTest 4 with %d threads.\n",i);
         PSnapshot test4Snapshot;
         if(createPSnapshot(&test4Snapshot,i,i,1) == EXIT_FAILURE){
@@ -209,18 +211,24 @@ int main(){
         }
 #pragma omp barrier
         freePSnapshot(&test4Snapshot);
+        for(int j = 0;j<i;j++) {
+            if(snap1[j] > snap4[j])
+                assert(false);
+            if(snap2[j] > snap3[j])
+                assert(false);
+        }
     }
 
     printf("\nTest 4 passed.\n\n");
 
 
     // Test 5:
-printf("Test 5: Snapshot made for 4,8,12 threads with randomly sized and valued registers.\n \
+printf("Test 5: Snapshot made for 4,8,12,..,64 threads with randomly sized and valued registers.\n \
 Thread 0 scans, thread 1 scans, sleeps for 10 milliseconds, scans again, finally thread 0 scans again.\n \
 Both threads scan a random number of registers, and the scanned registers are random \n \
 * Assertion: the second scan must have greater values than the first scan, element-wise.\n");
 
-for (int i = 4; i <= 12; i += 4) {
+for (int i = 4; i <= MAXTHRD; i += 4) {
     printf("\nTest 5 with %d threads.\n",i);
     PSnapshot test5Snapshot;
     if(createPSnapshot(&test5Snapshot, i, i, 0) == EXIT_FAILURE){
@@ -235,12 +243,16 @@ for (int i = 4; i <= 12; i += 4) {
     bool thread0Done = false;
     bool thread1Done = false;
     srand(time(NULL));
+    int reg_size_read_1 = 0;
+    int reg_size_read_2 = 0;
 
 #pragma omp barrier
-#pragma omp parallel default(none) shared(test5Snapshot,snap1,snap2,snap3,snap4,i,thread0Done,thread1Done) num_threads(i)
+#pragma omp parallel default(none) shared(test5Snapshot,snap1,snap2,snap3,snap4,i,thread0Done,thread1Done,reg_size_read_1,reg_size_read_2) num_threads(i)
     {
         int me = omp_get_thread_num();
         int registerSize = rand() % (i-2) + 1; // random size for the vector
+        if(omp_get_thread_num() == 0) reg_size_read_1 = registerSize;
+        if(omp_get_thread_num() == 1) reg_size_read_2 = registerSize;
         int registers[registerSize];
         for(int j = 0; j < registerSize; j++){
             registers[j] = rand() % (i-2) + 2; // put in the vector a random register
@@ -297,8 +309,18 @@ for (int i = 4; i <= 12; i += 4) {
             }
         }
     }
-#pragma omp barrier
-freePSnapshot(&test5Snapshot);
+    #pragma omp barrier
+    freePSnapshot(&test5Snapshot);
+
+    for(int j = 0;j<reg_size_read_1;j++) {
+    if(snap1[j] > snap4[j])
+        assert(false);
+    }
+    for(int j = 0;j<reg_size_read_2;j++) {
+    if(snap2[j] > snap3[j])
+        assert(false);
+    }
+
 }
 
 printf("\nTest 5 passed.\n\n");
